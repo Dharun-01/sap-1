2285096fa478e45c49d7a6ac6451f6b1f1ffc8cb
Dharun-01 <dharun01.dev@gmail.com>
2026-05-13 13:15:14 +0530

    Thoroughly chekced all the lib and submodules so far created and there is no bug

--- rtl/lib/tristate_buff_n/tristate_buff_n_tb.v
+++ rtl/lib/tristate_buff_n/tristate_buff_n_tb.v
@@ -1,4 +1,4 @@
-module tristate_buff_tb();
+module tristate_buff_n_tb();
 parameter W = 1;
 reg [W-1:0] tb_a;
 reg tb_en;
@@ -7,8 +7,8 @@ wire [W-1:0] tb_y;
 tristate_buff_n #(.WIDTH(W)) dut (.en(tb_en), .a(tb_a), .y(tb_y));
 
 initial begin 
-  $dumpfile("sim/tristate_buffer/tristate_buff.vcd");
-  $dumpvars(0, tristate_buff_tb);
+  $dumpfile("sim/tristate_buff_n/tristate_buff_n.vcd");
+  $dumpvars(0, tristate_buff_n_tb);
   tb_a = 0; tb_en = 0; #10;
   if (tb_y !== {W{1'bz}}) $display("tb_a_0 failed because en was 0");
   tb_a = 0; tb_en = 1; #10;
